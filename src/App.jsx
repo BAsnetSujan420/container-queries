2f28f9e185dbcc6d3060ba37170e460881ff3507
Sujan Basnet <basnetsujan1828420@gmail.com>
2026-03-11 11:49:00 +0545

    demo product card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import BasicCardGrid from './components/BasicCardGrid';
 import FeaturedArticles from './components/SmartLayout';
+import DemoCard from './components/DemoCard';
 
 function App() {
   return (
     <>
       {/* <BasicCardGrid /> */}
-      <FeaturedArticles />
+      {/* <FeaturedArticles /> */}
+      <DemoCard />
     </>
   );
 }
---
+++ src/components/DemoCard.jsx
@@ -0,0 +1,140 @@
+import styled from 'styled-components';
+
+const Page = styled.div`
+  --btn-bg: #7fffd0;
+  --btn-border: magenta;
+  --text: black;
+
+  font-family: system-ui, Roboto;
+  padding: 40px;
+`;
+
+const CardContainer = styled.div`
+  width: 100%;
+  max-width: 640px;
+`;
+
+const Card = styled.div`
+  padding: 1rem;
+  border: 3px solid var(--btn-bg);
+  text-align: center;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  display: block;
+`;
+
+const Content = styled.div``;
+
+const Title = styled.h3`
+  text-transform: uppercase;
+  color: magenta;
+  font-size: 1rem;
+`;
+
+const Price = styled.span`
+  display: block;
+  margin: 1rem 0;
+  font-style: italic;
+  color: #00b371;
+`;
+
+const Desc = styled.p``;
+
+const ButtonContainer = styled.div`
+  width: 100%;
+`;
+
+const CartButton = styled.button`
+  background: var(--btn-bg);
+  border: 2px dashed var(--btn-border);
+  box-shadow: 6px 6px 0 var(--btn-border);
+  max-width: 60px;
+  height: 3rem;
+  border-radius: 10px;
+`;
+
+const CartIcon = styled.svg`
+  width: 100%;
+  height: 100%;
+  margin-right: -0.5rem;
+
+  .plus-group > line {
+    stroke-width: 20px;
+    stroke: var(--text);
+    display: none;
+  }
+
+  .cart-lines-group > line,
+  .cart-group > polyline,
+  .cart-group > circle {
+    stroke-width: 10px;
+    stroke: var(--text);
+    fill: none;
+  }
+`;
+
+const CartText = styled.p`
+  text-transform: uppercase;
+  letter-spacing: 0.8px;
+  font-size: 18px;
+  line-height: 0;
+  padding: 0.25rem;
+
+  .add,
+  .to-cart {
+    display: none;
+  }
+`;
+
+export default function DemoCard() {
+  return (
+    <Page>
+      <CardContainer>
+        <Card>
+          <Image src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?w=800" />
+
+          <Content>
+            <Title>Neon Shapes Onesie</Title>
+            <Price>$16.99</Price>
+
+            <Desc>
+              The perfect onesie for your millenial baby. Peace, love, shapes
+              and bright colors bring this piece to life for all seasons.
+            </Desc>
+
+            <ButtonContainer>
+              <CartButton>
+                <CartIcon viewBox="0 0 320 173">
+                  <g className="plus-group">
+                    <line x1="278" y1="36" x2="278" y2="124" />
+                    <line x1="322" y1="80" x2="234" y2="80" />
+                  </g>
+
+                  <g className="cart-lines-group">
+                    <line x1="38" y1="68" x2="200" y2="68" />
+                    <line x1="83.54" y1="36.2" x2="95.69" y2="99.84" />
+                    <line x1="156.23" y1="36.2" x2="144.7" y2="99.84" />
+                  </g>
+
+                  <g className="cart-group">
+                    <polyline points="44 100 190.95 100 209.44 36 30.77 36 44.48 100" />
+                    <polyline points="185 131 51.44 131 44.78 99.91 24.46 5 0 5" />
+                    <circle cx="68" cy="152" r="16" />
+                    <circle cx="169" cy="152" r="16" />
+                  </g>
+                </CartIcon>
+
+                <CartText>
+                  <span className="add">Add</span>
+                  <span className="to-cart"> to cart</span>
+                </CartText>
+              </CartButton>
+            </ButtonContainer>
+          </Content>
+        </Card>
+      </CardContainer>
+    </Page>
+  );
+}
